1cff9abab3d1e5589f96fa807a3b531269e90df3
Isaac Guo <yue_guo@keysight.com>
2016-09-30 22:38:22 +0800

    switched to use https and redirect 8080 to 8443

--- server/hydrantmap/hydrantmap-project/src/main/java/com/isaac/hydrantmap/WebConfiguration.java
+++ server/hydrantmap/hydrantmap-project/src/main/java/com/isaac/hydrantmap/WebConfiguration.java
@@ -1,7 +1,13 @@
 package com.isaac.hydrantmap;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.h2.server.web.WebServlet;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -17,6 +23,34 @@ public class WebConfiguration {
 		registrationBean.addInitParameter("webAllowOthers", "true");
 		return registrationBean;
 	}
+	
+	  @Bean
+	  public EmbeddedServletContainerFactory servletContainer() {
+	    TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+	        @Override
+	        protected void postProcessContext(Context context) {
+	          SecurityConstraint securityConstraint = new SecurityConstraint();
+	          securityConstraint.setUserConstraint("CONFIDENTIAL");
+	          SecurityCollection collection = new SecurityCollection();
+	          collection.addPattern("/*");
+	          securityConstraint.addCollection(collection);
+	          context.addConstraint(securityConstraint);
+	        }
+	      };
+	    
+	    tomcat.addAdditionalTomcatConnectors(initiateHttpConnector());
+	    return tomcat;
+	  }
+	  
+	  private Connector initiateHttpConnector() {
+	    Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+	    connector.setScheme("http");
+	    connector.setPort(8080);
+	    connector.setSecure(false);
+	    connector.setRedirectPort(8443);
+	    
+	    return connector;
+	  }
 }
 
 @Configuration
