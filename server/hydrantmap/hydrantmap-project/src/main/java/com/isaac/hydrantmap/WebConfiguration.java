bd9853a93c5bcc20c24d0781505a20fdbad4720d
Isaac Guo <yue_guo@keysight.com>
2016-09-30 23:16:42 +0800

    secure web with a simple pari of username and password

--- server/hydrantmap/hydrantmap-project/src/main/java/com/isaac/hydrantmap/WebConfiguration.java
+++ server/hydrantmap/hydrantmap-project/src/main/java/com/isaac/hydrantmap/WebConfiguration.java
@@ -5,64 +5,88 @@ import org.apache.catalina.connector.Connector;
 import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.h2.server.web.WebServlet;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
-import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class WebConfiguration {
 	@Bean
-	ServletRegistrationBean h2servletRegistration()
-	{
-		ServletRegistrationBean registrationBean=new ServletRegistrationBean(new  WebServlet());
+	ServletRegistrationBean h2servletRegistration() {
+		ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
 		registrationBean.addUrlMappings("/console/*");
 		registrationBean.addInitParameter("webAllowOthers", "true");
 		return registrationBean;
 	}
-	
-	  @Bean
-	  public EmbeddedServletContainerFactory servletContainer() {
-	    TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
-	        @Override
-	        protected void postProcessContext(Context context) {
-	          SecurityConstraint securityConstraint = new SecurityConstraint();
-	          securityConstraint.setUserConstraint("CONFIDENTIAL");
-	          SecurityCollection collection = new SecurityCollection();
-	          collection.addPattern("/*");
-	          securityConstraint.addCollection(collection);
-	          context.addConstraint(securityConstraint);
-	        }
-	      };
-	    
-	    tomcat.addAdditionalTomcatConnectors(initiateHttpConnector());
-	    return tomcat;
-	  }
-	  
-	  private Connector initiateHttpConnector() {
-	    Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-	    connector.setScheme("http");
-	    connector.setPort(8080);
-	    connector.setSecure(false);
-	    connector.setRedirectPort(8443);
-	    
-	    return connector;
-	  }
+
+	@Bean
+	public EmbeddedServletContainerFactory servletContainer() {
+		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+			@Override
+			protected void postProcessContext(Context context) {
+				SecurityConstraint securityConstraint = new SecurityConstraint();
+				securityConstraint.setUserConstraint("CONFIDENTIAL");
+				SecurityCollection collection = new SecurityCollection();
+				collection.addPattern("/*");
+				securityConstraint.addCollection(collection);
+				context.addConstraint(securityConstraint);
+			}
+		};
+
+		tomcat.addAdditionalTomcatConnectors(initiateHttpConnector());
+		return tomcat;
+	}
+
+	private Connector initiateHttpConnector() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setScheme("http");
+		connector.setPort(8080);
+		connector.setSecure(false);
+		connector.setRedirectPort(8443);
+
+		return connector;
+	}
+}
+
+
+@Configuration
+class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+    }
+
 }
 
 @Configuration
-class SecurityConfiguration extends WebSecurityConfigurerAdapter
-{
+@EnableWebSecurity
+class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
 		// TODO Auto-generated method stub
-		httpSecurity.authorizeRequests().antMatchers("/").permitAll().and().authorizeRequests().antMatchers("/console/**").permitAll();
-		httpSecurity.csrf().disable();
-		httpSecurity.headers().frameOptions().disable();
+		// httpSecurity.authorizeRequests().antMatchers("/").permitAll().and().authorizeRequests().antMatchers("/console/**").permitAll();
+		// httpSecurity.csrf().disable();
+		// httpSecurity.headers().frameOptions().disable();
+
+		httpSecurity.authorizeRequests().antMatchers("/", "/home", "/console/**").permitAll().anyRequest()
+				.authenticated().and().formLogin().loginPage("/login").permitAll().and().logout().permitAll();
+
 	}
-	
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("hydrant").password("p4ssw0rd").roles("USER");
+	}
+
 }
