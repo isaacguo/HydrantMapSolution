5c49ffdefa2b2f91ba54786bd00bbf3ae10b482e
Isaac Guo <yue_guo@keysight.com>
2016-10-01 17:51:23 +0800

    disable https, added map webpage

--- server/hydrantmap/hydrantmap-project/src/main/java/com/isaac/hydrantmap/WebConfiguration.java
+++ server/hydrantmap/hydrantmap-project/src/main/java/com/isaac/hydrantmap/WebConfiguration.java
@@ -27,7 +27,7 @@ public class WebConfiguration {
 		registrationBean.addInitParameter("webAllowOthers", "true");
 		return registrationBean;
 	}
-
+/*
 	@Bean
 	public EmbeddedServletContainerFactory servletContainer() {
 		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
@@ -46,6 +46,7 @@ public class WebConfiguration {
 		return tomcat;
 	}
 
+	
 	private Connector initiateHttpConnector() {
 		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
 		connector.setScheme("http");
@@ -55,6 +56,7 @@ public class WebConfiguration {
 
 		return connector;
 	}
+	*/
 }
 
 
@@ -64,9 +66,12 @@ class MvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/map").setViewName("map");
     }
 
 }
+
+
 
 @Configuration
 @EnableWebSecurity
@@ -74,19 +79,23 @@ class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
-		// TODO Auto-generated method stub
-		// httpSecurity.authorizeRequests().antMatchers("/").permitAll().and().authorizeRequests().antMatchers("/console/**").permitAll();
-		// httpSecurity.csrf().disable();
-		// httpSecurity.headers().frameOptions().disable();
+		 //TODO Auto-generated method stub
+		 httpSecurity.authorizeRequests().antMatchers("/").permitAll().and().authorizeRequests().antMatchers("/console/**").permitAll();
+		 httpSecurity.csrf().disable();
+		 httpSecurity.headers().frameOptions().disable();
 
+		/*
 		httpSecurity.authorizeRequests().antMatchers("/", "/home", "/console/**").permitAll().anyRequest()
 				.authenticated().and().formLogin().loginPage("/login").permitAll().and().logout().permitAll();
+				*/
 
 	}
 
+	/*
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication().withUser("hydrant").password("p4ssw0rd").roles("USER");
 	}
+	*/
 
 }
